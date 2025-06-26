93a6360dd3e79f5d58d0c5aa9490895b9a4b50a4
n1x9s <spl1ntero@yandex.ru>
2025-06-27 02:36:51 +0300

    Initial Commit

---
+++ sdvg/sdvgApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdvgApp.swift
+//  sdvg
+//
+//  Created by Nikita Sergeev on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct sdvgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sdvgTests/sdvgTests.swift
@@ -0,0 +1,17 @@
+//
+//  sdvgTests.swift
+//  sdvgTests
+//
+//  Created by Nikita Sergeev on 27.06.2025.
+//
+
+import Testing
+@testable import sdvg
+
+struct sdvgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
